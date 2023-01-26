80c3adc68d0840a044d69e7de3788f2e369f5b8f
krisiilieva <kristin.ilieva18@gmail.com>
2023-01-26 13:12:32 +0100

    I started making the in-profile applicants page

--- src/Pages/Small Pages/applicantsProfilePage.jsx
+++ src/Pages/Small Pages/applicantsProfilePage.jsx
@@ -1,3 +1,31 @@
 export default function ApplicantInProfilePage() {
-  return <></>;
+  return (
+    <>
+    <div className="in-applicants-profile-page-outer">
+      <div className="in-applicants-profile-page-inner">
+        <div className="in-applicants-profile-page-notes">
+          <p>Notes</p>
+        </div>
+        <div className="in-applicants-profile-page-notes">
+          <p>Notes</p>
+        </div>
+        <div className="in-applicants-profile-page-notes">
+          <p>Notes</p>
+        </div>
+      </div>
+    </div>
+    <div className=" in-applicant-profile-picture-holder">
+        <img src="https://assets.citynavigator.nl/thumb/p3tsSJLLYpsERDYz8kA1puN81iy5OB_1v71l2zLtD-c/resizing_type:fit/width:960/height:0/gravity:sm/enlarge:0/aHR0cHM6Ly9hc3NldHMuY2l0eW5hdmlnYXRvci5ubC9rdW1hLWl0Yy91cGxvYWRzL21lZGlhLzYxNmVjOWNjZGJjNjAvc2h1dHRlcnN0b2NrLWdlc3BpZWdlbGQuanBn.jpg" alt=""
+                className="in-applicant-profile-picture"/>
+    </div>
+    <div className="in-applicant-profile-information-holder-outer">
+      <div className="in-applicant-profile-information-holder-inner">
+         <p>Meow</p>
+         <p>Meow</p>
+         <p>Meow</p>
+         <p>Meow</p>
+      </div>
+    </div>
+    </>
+    );
 }
